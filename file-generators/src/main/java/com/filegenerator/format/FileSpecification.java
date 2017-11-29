3fce838e4f8eb1a505baffeca60aed8e740c822d
Sander <sander.han@gmail.com>
2017-11-29 00:09:55 -0800

    add field specs

--- file-generators/src/main/java/com/filegenerator/format/FileSpecification.java
+++ file-generators/src/main/java/com/filegenerator/format/FileSpecification.java
@@ -1,5 +1,13 @@
 package com.filegenerator.format;
 
+import lombok.Data;
+
+@Data
 public class FileSpecification {
+	public static enum Lookup {
+		SEQUENCE,NAME
+	}
+	
+	Lookup lookup = Lookup.SEQUENCE;
 	
 }
